0dee70223850a0cf4de44d8502adf142070cbd89
arshamarg9 <arshamghaderiserpents@gmail.com>
2024-03-01 04:19:51 +0330

    Edit on script(script 2)

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const inputBox = document.getElementById('input-box');
 const colorBox = document.getElementById('color-box');
 const link = document.querySelector('.btn');
 
+link.addEventListener('click', () => {
+    title.textContent = inputBox.value;
+});
